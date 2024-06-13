9ec4cd6d26bb9813005475621999e8e91cd72037
devops <devops@mobilize.net>
2024-06-13 13:36:26 -0600

    Updated Code with latest version 7976d565168ce3007590f8d92fc38040785de7b2

---
+++ Stubs/System.Windows.Forms/ControlEx.cs
@@ -0,0 +1,13 @@
+namespace Stub._System.Windows.Forms
+{
+
+   public class ControlEx
+   {
+
+      public Mobilize.Web.Cursor Cursor { get; set; }
+
+      public Mobilize.Web.ImeMode ImeMode { get; set; }
+
+   }
+
+}
